daae1d8f48c70652f4443d0a72666cc605880f21
rickert156 <166800971+rickert156@users.noreply.github.com>
2024-06-03 12:06:20 +0400

    Update glue.py

--- glue.py
+++ glue.py
@@ -17,18 +17,18 @@ try:
     document = (list_dirs[enter_list])
     print(document)
 
-    with open(document, 'r') as file_csv:
+    with open('test.csv', 'r') as file_csv:
         for row in csv.DictReader(file_csv):
             row_name = row['name']
             row_domain = row['domain']
             row_company = row['company']
             if ' ' in row_name:
                 first_name, last_name = row_name.split(' ')
-                option_1 = f'{first_name.lower()}.{last_name.lower()}@{row_domain}'
-                option_2 = f'{last_name.lower()}.{first_name.lower()}@{row_domain}'
-                option_3 = f'{first_name.lower()}{last_name.lower()}@{row_domain}'
-                option_4 = f'{last_name.lower()}{first_name.lower()}@{row_domain}'
-                option_5 = f'{first_name.lower()}@{row_domain}'
+                option_1 = f'{first_name.lower()}.{last_name.lower()}@{row_domain}'#ok
+                option_2 = f'{first_name.lower()}{last_name.lower()}@{row_domain}'#ok
+                option_3 = f'{first_name.lower()}@{row_domain}'#ok
+                option_4 = f'{first_name[0].lower()}.{last_name.lower()}@{row_domain}'#ok
+                option_5 = f'{first_name[0].lower()}{last_name.lower()}@{row_domain}'#ok
                 print(option_1, row_company)
                 print(option_2, row_company)
                 print(option_3, row_company)
@@ -36,7 +36,7 @@ try:
                 print(option_5, row_company)
                 column = f'{row_name},{row_company}'
                 with open('result.csv', 'a+') as result_file:
-                    result_file.write(f'{column},{option_1}\n{column},{option_2}\n{column},{option_3}\n{column},{option_4}\n{column},{option_5}')
+                    result_file.write(f'{column},{option_1}\n{column},{option_2}\n{column},{option_3}\n{column},{option_4}\n{column},{option_5}\n')
             else:
                 result_email = f'{row_name.lower()}@{row_domain}'
                 print(result_email, row_company)
